f5e24bd7d8643c19484d06b8dae528fd9452d111
Yohannes <yohannesfantahun88@gmail.com>
2025-01-21 14:40:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import About from "./pages/About";
+import Landing from "./pages/Landing";
+import Error from "./pages/Error";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
+import Register from "./pages/Register";
+import HomeLayout from "./pages/HomeLayout";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-xl font-bold underline text-red-400">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
